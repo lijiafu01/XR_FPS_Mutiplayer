b79a1548c26b405adbe1cdc69626f87d5cc9be2e
lijiafu01 <nguyenviethoan5555@gmail.com>
2024-05-07 02:14:09 +0800

    add chen wei cheng

---
+++ Assets/_ChenWeiCheng/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Di chuyển ô tô theo trục x và z
+        float moveHorizontal = Input.GetAxis("Horizontal");//A D move
+        float moveVertical = Input.GetAxis("Vertical");//WS move
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+}
